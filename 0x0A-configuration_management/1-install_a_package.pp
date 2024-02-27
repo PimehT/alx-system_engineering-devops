3021198687d13ff7b3df6fc93752f65955e8350d
PimehT <tareerepimeh@gmail.com>
2024-02-27 23:00:15 +0100

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,8 @@
 # Use puppet to install the software package Flask 2.1.0 from pip3
 
-exec {
+exec {'install_flask':
   command     => '/usr/bin/pip3 install Flask==2.1.0',
   path        => ['/usr/bin'],
   environment => ['HOME=/root'],
+  require     => Package['python3-pip'],
 }
