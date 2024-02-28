cd52a11963695d6979ee757c4648e9f2cc10ec9d
PimehT <tareerepimeh@gmail.com>
2024-02-28 13:55:57 +0100

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,17 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+package { 'python3-pip':
+  ensure   => installed,
+}
+
+package { 'werkzeug':
+  ensure    => '2.1.1',
+  provider  => 'pip3',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['usr/bin'],
+}
