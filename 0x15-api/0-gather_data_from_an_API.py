4fb1aaf9304338e98bc78a04b34682538502aab5
PimehT <tareerepimeh@gmail.com>
2024-03-27 00:43:57 +0100

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,19 @@
-#!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+#!/usr/bin/python3``
+"""
+returns information about his/her TO_DO list progress.
+"""
 import requests
-import sys
+from sys import argv
+
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    user_id = int(argv[1])
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    user = requests.get(url).json()
+    url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+        user_id)
+    todos = requests.get(url).json()
+    completed = [task for task in todos if task.get('completed') is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+        user.get('name'), len(completed), len(todos)))
+    [print("\t {}".format(task.get('title'))) for task in completed]
