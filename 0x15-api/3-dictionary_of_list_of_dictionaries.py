65017fe6d19fb94f50afa41e5e95027eb82daab4
PimehT <tareerepimeh@gmail.com>
2024-03-27 03:51:21 +0100

    api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,33 +3,23 @@
 returns information about his/her TO_DO list progress.
 """
 import json
+import re
 import requests
-
-
-def export_all_to_json():
-    """
-    Exports all tasks from all employees to a JSON file.
-    """
-    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
-
-    user_dict = {}
-    for user in users:
-        user_id = user.get('id')
-        user_name = user.get('username')
-        user_tasks = []
-        for task in todos:
-            if task.get('userId') == user_id:
-                user_tasks.append({
-                    "username": user_name,
-                    "task": task.get('title'),
-                    "completed": task.get('completed')
-                })
-        user_dict[user_id] = user_tasks
-
-    with open('todo_all_employees.json', 'w') as json_file:
-        json.dump(user_dict, json_file)
+from sys import argv
 
 
 if __name__ == "__main__":
-    export_all_to_json()
+    url = "https://jsonplaceholder.typicode.com"
+    if re.fullmatch(r'\d+', argv[1]):
+        id = int(argv[1])
+
+        user = requests.get(f'{url}/users/{id}').json()
+        todos = requests.get(f'{url}/todos').json()
+
+        with open(f'todo_all_employees.json', 'w') as file:
+            user_name = user.get('username')
+            json.dump({id: [{
+                "username": user_name,
+                "task": task.get('title'),
+                "completed": task.get('completed')
+            } for task in todos]}, file)
