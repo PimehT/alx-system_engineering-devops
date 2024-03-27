aff3f984641c8051e7fb25042f565dea27a3ae66
PimehT <tareerepimeh@gmail.com>
2024-03-27 02:07:21 +0100

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3``
+#!/usr/bin/python3
 """
 returns information about his/her TO_DO list progress.
 """
@@ -13,10 +13,11 @@ if __name__ == "__main__":
         user_id = int(argv[1])
         user = requests.get(f'{url}/users/{user_id}').json()
         user_name = user.get('name')
-        todos = requests.get(f'{url}/todos?userId={user_id}').json()
-        completed = [task for task in todos if task.get('completed') is True]
+        todos = requests.get(f'{url}/todos').json()
+        user_todos = [task for task in todos if task.get('userId') == user_id]
+        completed = [task for task in user_todos if task.get('completed') is True]
         print("Employee {} is done with tasks({}/{}):".format(
-            user_name, len(completed), len(todos)
+            user_name, len(completed), len(user_todos)
         ))
         for task in completed:
             print("\t {}".format(task.get('title')))
