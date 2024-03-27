af91934b2641f4eb4757c94ab994783af803007e
PimehT <tareerepimeh@gmail.com>
2024-03-27 03:27:32 +0100

    api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3``
+#!/usr/bin/python3
 """
 returns information about his/her TO_DO list progress.
 export data in the CSV format.
@@ -9,22 +9,21 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    API = "https://jsonplaceholder.typicode.com"
+    url = "https://jsonplaceholder.typicode.com"
     if re.fullmatch(r'\d+', argv[1]):
-        id = int(argv[1])
+        user_id = int(argv[1])
 
-        usr_json = requests.get(f'{API}/users/{id}').json()
-        todos_json = requests.get(f'{API}/todos').json()
+        user = requests.get(f'{url}/users/{user_id}').json()
+        todos = requests.get(f'{url}/todos').json()
+        user_name = user.get('username')
+        user_todos = [task for task in todos if task.get('userId') == id]
 
-        user_name = usr_json.get('username')
-        usr_todos = [task for task in todos_json if task.get('userId') == id]
-
-        with open(f'{id}.csv', 'w') as file:
-            for task in usr_todos:
+        with open(f'{user_id}.csv', 'w') as file:
+            for task in user_todos:
                 task_status = task.get('completed')
                 task_title = task.get('title')
                 file.write('"{}","{}","{}","{}"\n'.format(
-                                                        id,
+                                                        user_id,
                                                         user_name,
                                                         task_status,
                                                         task_title
