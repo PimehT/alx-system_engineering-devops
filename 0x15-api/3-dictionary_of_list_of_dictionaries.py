1e34b66f7c1f77bf9a3bf3701d498dbf4bd82ea8
PimehT <tareerepimeh@gmail.com>
2024-03-27 04:07:15 +0100

    api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,27 +1,21 @@
-#!/usr/bin/python3
-"""
-returns information about his/her TO_DO list progress.
-"""
+import requests
 import json
 import re
-import requests
-from sys import argv
-
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com"
-    if re.fullmatch(r'\d+', argv[1]):
-        id = int(argv[1])
-
-        users = requests.get(f'{url}/users').json()
+    users = requests.get(f'{url}/users').json()
+    formatted_data = {}
+    for user in users:
+        user_id = user['id']
+        user_name = user['username']
         todos = requests.get(f'{url}/todos').json()
-
-        todo_dict = {}
-        for user in users:
-            user_id = user.get('id')
-            user_name = user.get('username')
-            user_tasks = [{"username": user_name, "task": task.get('title'), "completed": task.get('completed')} for task in todos if task.get('userId') == user_id]
-            todo_dict[user_id] = user_tasks
-
-        with open('todo_all_employees.json', 'w') as file:
-            json.dump(todo_dict, file)
+        user_todos = [task for task in todos if task.get('userId') == user_id]
+        formatted_todos = [{
+            "username": user_name,
+            "task": task.get('title'),
+            "completed": task.get('completed')
+        } for task in user_todos]
+        formatted_data[user_id] = formatted_todos
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(formatted_data, file, indent=4)
