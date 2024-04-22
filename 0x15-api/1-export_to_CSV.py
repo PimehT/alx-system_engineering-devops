85e9c1acbc47ad1016b2f98273e650d1fc1533e4
PimehT <tareerepimeh@gmail.com>
2024-04-22 19:18:24 +0100

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,26 +3,25 @@
 returns export data in the CSV format of
 information about his/her TO_DO list progress.
 """
-import re
+import csv
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com"
-    if re.fullmatch(r'\d+', argv[1]):
-        id = int(argv[1])
+    if len(argv) == 2 or argv[1].isdigit():
+        id = argv[1]
+        url = 'https://jsonplaceholder.typicode.com/'
+        user = requests.get(url + 'users/' + id).json()
+        todos = requests.get(url + 'todos/').json()
+        tasks = [todo for todo in todos if todo.get('userId') == int(id)]
+        completed = [done for done in tasks if done.get('completed') is True]
 
-        user = requests.get(f'{url}/users/{id}').json()
-        todos = requests.get(f'{url}/todos').json()
-
-        user_name = user.get('username')
-        user_todos = [task for task in todos if task.get('userId') == id]
-
-        with open(f'{id}.csv', 'w') as file:
-            for task in user_todos:
-                task_status = task.get('completed')
-                task_title = task.get('title')
-                file.write('"{}","{}","{}","{}"\n'.format(
-                    id, user_name, task_status, task_title
-                ))
+        with open(f'{id}.csv', 'w', newline='') as file:
+            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+            for task in tasks:
+                writer.writerow([
+                    id,
+                    user.get('username'),
+                    task.get('completed'),
+                    task.get('title')
+                ])
