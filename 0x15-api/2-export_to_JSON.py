cd7c38f580ed92647d152f29b3f654a9d0bf05a3
PimehT <tareerepimeh@gmail.com>
2024-04-22 19:31:05 +0100

    Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,27 +4,22 @@ returns information about his/her TO_DO list progress.
 export data to a JSON file.
 """
 import json
-import re
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com"
-    if re.fullmatch(r'\d+', argv[1]):
-        id = int(argv[1])
-
-        user = requests.get(f'{url}/users/{id}').json()
-        todos = requests.get(f'{url}/todos').json()
-
-        user_name = user.get('username')
-        user_todos = [task for task in todos if task.get('userId') == id]
-        completed = [task for task in user_todos
-                     if task.get('completed') is True]
+    if len(argv) == 2 or argv[1].isdigit():
+        id = argv[1]
+        url = 'https://jsonplaceholder.typicode.com/'
+        user = requests.get(f'{url}users/{id}').json()
+        todos = requests.get(f'{url}todos/').json()
+        tasks = [todo for todo in todos if todo.get('userId') == int(id)]
+        completed = [done for done in tasks if done.get('completed') is True]
 
         with open(f'{id}.json', 'w') as file:
             json.dump({id: [{
                 "task": task.get('title'),
                 "completed": task.get('completed'),
-                "username": user_name
-            } for task in user_todos]}, file)
+                "username": user.get('name'),
+            } for task in tasks]}, file)
