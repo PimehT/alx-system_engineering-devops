8b1b8580f38825b930f4f84622aabbff177f56e0
PimehT <tareerepimeh@gmail.com>
2024-05-08 21:58:15 +0100

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
 """
-Module 0-subs that contains the function number_of_subscribers
+Module for number_of_subscribers function
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and returns the number of
-    subscribers (not active users, total subscribers) for a given
-    subreddit. If an invalid subreddit is given, the function should
-    return 0)
     """
-    if not subreddit or type(subreddit) is not str:
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit. If an invalid subreddit is given, the
+    function should return 0.
+    """
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
         return 0
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0'}
-    r = requests.get(url, headers=headers)
-    if r.status_code == 200:
-        r = r.json()
-    else:
-        return 0
-    return r.get('data', {}).get('subscribers', 0)
+
+    return response.json().get('data', {}).get('subscribers', 0)
