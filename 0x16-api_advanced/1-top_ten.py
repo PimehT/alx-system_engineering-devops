e380839c43f6490e445dc96ff08bebf970d9ebec
PimehT <tareerepimeh@gmail.com>
2024-05-08 23:07:47 +0100

    0x16-api_advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,7 +11,7 @@ def top_ten(subreddit):
     """
     app_name = '0x16-api_advanced'
     user_name = 'FeelingPsychology300'
-    headers = {'User-Agent': f'{app_name}/0.0.1 by /u/{user_name}'}
+    headers = {'User-Agent': f'{app_name}/0.0.1 by (/u/{user_name})'}
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
