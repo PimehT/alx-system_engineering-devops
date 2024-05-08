7d98dab5b01b811f8f391f39c918cfb262c4a85e
PimehT <tareerepimeh@gmail.com>
2024-05-08 22:54:36 +0100

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,7 @@ def top_ten(subreddit):
     Top Ten popular posts of a given subreddit
     """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Python/requests:APIproject:0.1 (by /u/vegaisaias)'}
+    headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
