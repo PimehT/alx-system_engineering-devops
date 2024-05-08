d9731a91220c90ca180a254d28c48e9c7bae124e
PimehT <tareerepimeh@gmail.com>
2024-05-08 21:29:04 +0100

    0x16-api_advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module for number_of_subscribers function
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit. If an invalid subreddit is given, the
+    function should return 0.
+    """
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+
+    return response.json().get('data', {}).get('subscribers', 0)
