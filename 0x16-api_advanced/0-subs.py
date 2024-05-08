683d59bb2ed88a462c64207e207ce3ef13267102
PimehT <tareerepimeh@gmail.com>
2024-05-09 00:12:38 +0100

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,13 +11,14 @@ def number_of_subscribers(subreddit):
     for a given subreddit. If an invalid subreddit is given, the
     function should return 0.
     """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     app_name = '0x16-api_advanced'
     user_name = 'FeelingPsychology300'
     headers = {'User-Agent': f'{app_name}/0.0.1 by /u/{user_name}'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
         return 0
 
-    return response.json().get('data', {}).get('subscribers', 0)
+    result = response.json().get('data', {})
+    return result.get('subscribers', 0)
