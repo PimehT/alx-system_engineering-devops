77c03aa4fc3fb5c3031f29ad2fe09b4c67ed40ed
PimehT <tareerepimeh@gmail.com>
2024-05-14 17:29:03 +0100

    0x19-postmortem

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,45 @@
-# catch the error and fix it
-exec {'/etc/php5/apache2/php.ini':
-  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
-  command => "sed -i 's/display_errors = Off/display_errors = On/g' /etc/php5/apache2/php.ini",
+# Define the Apache configuration variables
+$apache_lock_dir = "/var/lock/apache2"
+$apache_pid_file = "/var/run/apache2.pid"
+$apache_run_user = "www-data"
+$apache_run_group = "www-data"
+$apache_log_dir = "/var/log/apache2"
+
+# Ensure the lock directory exists
+file { $apache_lock_dir:
+  ensure => directory,
+  owner  => $apache_run_user,
+  group  => $apache_run_group,
+  mode   => '0755',
 }
 
-exec { '/var/www/html/wp-setting.php':
-  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
-  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+# Ensure the PID file directory exists
+file { File.dirname($apache_pid_file):
+  ensure => directory,
+  owner  => $apache_run_user,
+  group  => $apache_run_group,
+  mode   => '0755',
 }
 
-exec {'/etc/init.d/apache2':
-  path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-  command => '/etc/init.d/apache2 restart',
+# Ensure the log directory exists
+file { $apache_log_dir:
+  ensure => directory,
+  owner  => $apache_run_user,
+  group  => $apache_run_group,
+  mode   => '0755',
+}
+
+# Ensure Apache is configured with the correct variables
+template { '/etc/apache2/apache2.conf':
+  source => 'puppet:///modules/your_module/apache2.conf.erb',
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0644',
+  notify => Service['apache2'],
+}
+
+# Ensure the Apache service is enabled and running
+service { 'apache2':
+  ensure => running,
+  enable => true,
 }
