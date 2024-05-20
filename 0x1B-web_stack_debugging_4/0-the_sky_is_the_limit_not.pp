b9e89b2bbfb73e3edb9c6d0e5673741aaf4e742c
PimehT <tareerepimeh@gmail.com>
2024-05-20 18:29:57 +0100

    0x1B-web_stack_debugging_4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+exec { 'Update ulimit in nginx default config':
+  command => 'sed -i "/^ulimit=-n/d;/^ulimit=/a ulimit=-n 4096" /etc/default/nginx',
+  path    => ['/usr/bin', '/bin'],
+}
+
+exec { 'Restart nginx':
+  command => 'service nginx restart',
+  path    => ['/usr/bin', '/bin'],
+  require => Exec['Update ulimit in nginx default config'],
+} 
