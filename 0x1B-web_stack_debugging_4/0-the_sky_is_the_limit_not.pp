a4d66c56940783ad32362072b8d474a3a02bdd1e
PimehT <tareerepimeh@gmail.com>
2024-05-20 18:34:32 +0100

    0x1B-web_stack_debugging_4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,4 @@
+# fix the nginx configuration
 exec { 'Update ulimit in nginx default config':
   command => 'sed -i "/^ulimit=-n/d;/^ulimit=/a ulimit=-n 4096" /etc/default/nginx',
   path    => ['/usr/bin', '/bin'],
